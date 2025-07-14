de58f9f88c1d253aab29a8138dba22a631430184
Raihan <raihanuddinahmed10@gmail.com>
2025-07-14 23:06:47 +0600

    minor update

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
 document.getElementById("print-button").addEventListener("click", function () {
   window.print();
 });
+
+const date = new Date();
+const year = date.getFullYear();
+const month = (date.getMonth() + 1).toString().padStart(2, "0");
+const day = date.getDate().toString().padStart(2, "0");
+console.log(day, month, year);
+
+document.getElementById("today-date").textContent = `${day}/${month}/${year}`;
